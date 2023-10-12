7d4298f856ee3f8a603988e2f04ba0c0ca51538d
Adham <Adham-461@hotmail.com>
2023-10-12 03:13:49 +0300

    Initial commit: Project setup and full code upload

---
+++ assets/js/ar-main.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('booking-form');
+    
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const phonenumber = document.getElementById('phonenumber').value;
+        const appointment = document.getElementById('appointment').value;
+        const terms = document.getElementById('terms').checked;
+
+        if (!name || !email || !phonenumber || !appointment || !terms) {
+            Swal.fire({
+                icon: 'error',
+                title: 'خطأ',
+                text: 'يرجى التأكد من إدخال المعلومات لجميع الحقول والموافقة على الشروط والأحكام',
+                confirmButtonText: 'موافق'
+            });
+            return;
+        }
+
+        var templateParams = {
+            name: name,
+            phonenumber: phonenumber,
+            email: email,
+            appointment: appointment,
+        };
+        emailjs.send('service_syupqnu', 'template_lu89yob' ,templateParams)
+            .then(function(response) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'تم بنجاح',
+                    text: 'شكراً لتواصلك معنا, سنعود إليك في أقرب وقت ممكن!',
+                });
+                form.reset();
+            })
+            .catch(function(error) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'خطأ',
+                    text: 'حدث خطأ أثناء إرسال المعلومات يرجى المحاولة من جديد',
+                });
+            });
+    });
+});
+(function() {
+    emailjs.init('wNP4GhSKjfusHOz68');
+})();
---
+++ assets/js/en-main.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('booking-form');
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const phonenumber = document.getElementById('phonenumber').value;
+        const appointment = document.getElementById('appointment').value;
+        const terms = document.getElementById('terms').checked;
+
+        if (!name || !email || !phonenumber || !appointment || !terms) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Please make sure to fill in all fields and agree to the terms and conditions',
+                confirmButtonText: 'OK'
+            });
+            return;
+        }
+
+        var templateParams = {
+            name: name,
+            phonenumber: phonenumber,
+            email: email,
+            appointment: appointment,
+        };
+        emailjs.send('service_syupqnu', 'template_lu89yob' ,templateParams)
+            .then(function(response) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Success',
+                    text: 'Thank you for reaching out to us. We will get back to you as soon as possible!',
+                });
+                form.reset();
+            })
+            .catch(function(error) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error',
+                    text: 'An error occurred while sending the information. Please try again.',
+                });
+            });
+    });
+});
+(function() {
+    emailjs.init('wNP4GhSKjfusHOz68');
+})();
